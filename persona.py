9cdbbe8e23a301559af70c39ed33fa41af859a60
Sandra <sandra.silvanovera@gmail.com>
2019-04-24 18:07:17 -0400

    Se agregó el cumpleaños

--- persona.py
+++ persona.py
@@ -5,12 +5,28 @@
 
 class Persona:
     nombre = None
-    def __init__(self, un_nombre):
+    edad = 27
+    def __init__(self, un_nombre, cumple):
         self.nombre = un_nombre
-        print("Soy una persona que se llama", self.nombre,)
+        self.edad = cumple
+        print("Me llamo", self.nombre,"y tengo", self.edad, "años")
+    
+    def get_edad(self):
+        return self.edad
+    
+    def set_edad(self, cantidad):
+        self.edad = cantidad
 
-Pepito = Persona("Caesar")
-Pepita = Persona("Nico")
+    def cumpleaños(self):
+        self.edad = self.edad + 1
+        print("Me llamo", self.nombre,"y tengo", self.edad, "años")
+
+Individuo = Persona("Caesar", 27)
+
+# Modificar la clase Persona, agregarle un atributo edad 
+# y un método cumpleaños y un método get_edad
+# Inicializar/crear un objeto de tipo Persona y hacerle cumplir años
+
 
 
 
