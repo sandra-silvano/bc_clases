1198f21ac44d20f282f33656aa88a714ebd6a5f0
Sandra <sandra.silvanovera@gmail.com>
2019-04-24 16:26:35 -0400

    Se creó el objeto persona

--- persona.py
+++ persona.py
@@ -1,6 +1,16 @@
-# Esto es una prueba 
-print("Que tal Mundo?")
-print("Hola")
-print("Algo")
-print("Algo2")
+# Ejercicio
+
+# En el archivo persona.py crear una clase Persona con atributo nombre. 
+# Después instanciar un objeto de tipo persona
+
+class Persona:
+    nombre = None
+    def __init__(self, un_nombre):
+        self.nombre = un_nombre
+        print("Soy una persona que se llama", self.nombre,)
+
+Pepito = Persona("Caesar")
+Pepita = Persona("Nico")
+
+
 
